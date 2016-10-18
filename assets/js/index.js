234f43841ba94278b9bb702ea9b48907951e5dc6
Asa Smith <smith.asa.la@gmail.com>
2016-10-18 17:29:22 -0400

    add shipping info banner

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,14 @@
 console.log('you can use ES6 here : )')
+
+var clickTarget = document.getElementById('shipping-info');
+var shippingInfo = document.getElementsByTagName('span')[1];
+
+clickTarget.addEventListener('click', visToggle, false);
+
+function visToggle(){
+  shippingInfo.classList.toggle('visible');
+}
+
+
+console.log(clickTarget);
+console.log(shipping);
