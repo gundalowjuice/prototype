2eda9ec50237e8780a7ef88e6a869f18cadf5e39
Asa Smith <smith.asa.la@gmail.com>
2016-10-21 14:54:12 -0400

    add jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,18 @@
-console.log('you can use ES6 here : )')
+// console.log('you can use ES6 here : )')
+//
+// var clickTarget = document.getElementById('shipping-info');
+// var shippingInfo = document.getElementsByTagName('span')[1];
+//
+// clickTarget.addEventListener('click', visToggle, false);
+//
+// function visToggle(){
+//   shippingInfo.classList.toggle('visible');
+// }
+//
+//
+// console.log(clickTarget);
+// console.log(shipping);
 
-var clickTarget = document.getElementById('shipping-info');
-var shippingInfo = document.getElementsByTagName('span')[1];
-
-clickTarget.addEventListener('click', visToggle, false);
-
-function visToggle(){
-  shippingInfo.classList.toggle('visible');
-}
-
-
-console.log(clickTarget);
-console.log(shipping);
+$(document).ready(function(){
+  console.log('jquery is working!');
+});
