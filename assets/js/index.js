29a7b291a40e2301cb667642771f9cd554d61ebb
Asa Smith <smith.asa.la@gmail.com>
2016-10-22 11:36:37 -0400

    add shipping info banner (#5)
    * add shipping info banner
    
    * add jquery
    
    * add padding to shipping info copy
    
    * click event to show expanded shipping info on small screens
    
    * change span to paragraph tags

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
-console.log('you can use ES6 here : )')
+
+$(document).ready(function(){
+  checkDisplay();
+});
+
+function checkDisplay(){
+  if($('p:nth-of-type(2)').css('display') === 'none'){
+    $('#shipping-info').on('click', function () {
+        $('p:nth-of-type(2)').slideToggle('slow');
+    })
+  };
+};
