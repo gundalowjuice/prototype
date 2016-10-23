31ab49c14a2dcd0712a80e4f41738bff9535e21d
Asa Smith <smith.asa.la@gmail.com>
2016-10-22 22:31:16 -0400

    add slideToggle for nav dropdown

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,24 @@
 console.log('you can use ES6 here : )')
 
-var hamburger = document.getElementById('hamburger');
-var menu = document.getElementById('menu');
-
-hamburger.addEventListener('click', menuToggle, false);
-
-function menuToggle(){
-  menu.classList.toggle('hide');
-  console.log('toggled');
-};
-
-console.log(menu.classList);
-console.log(hamburger);
+// var hamburger = document.getElementById('hamburger');
+// var menu = document.getElementById('menu');
+//
+// hamburger.addEventListener('click', menuToggle, false);
+//
+// function menuToggle(){
+//   menu.classList.toggle('hide');
+//   console.log('toggled');
+// };
+//
+// console.log(menu.classList);
+// console.log(hamburger);
 
 $(document).ready(function(){
   checkDisplay();
+
+  $('#hamburger').click(function(){
+    $('#menu').slideToggle('slow');
+  })
 });
 
 function checkDisplay(){
