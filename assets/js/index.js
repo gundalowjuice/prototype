e0eb8e9d57eeaea8b4e8108cf22cc9ebcb8e2981
Asa Smith <smith.asa.la@gmail.com>
2016-10-25 14:26:14 -0400

    main nav drop down menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,30 @@
 console.log('you can use ES6 here : )')
+
+// var hamburger = document.getElementById('hamburger');
+// var menu = document.getElementById('menu');
+//
+// hamburger.addEventListener('click', menuToggle, false);
+//
+// function menuToggle(){
+//   menu.classList.toggle('hide');
+//   console.log('toggled');
+// };
+//
+// console.log(menu.classList);
+// console.log(hamburger);
+
+$(document).ready(function(){
+  checkDisplay();
+
+  $('#hamburger').click(function(){
+    $('#menu').fadeToggle('fast');
+  })
+});
+
+function checkDisplay(){
+  if($('p:nth-of-type(2)').css('display') === 'none'){
+    $('#shipping-info').on('click', function () {
+        $('p:nth-of-type(2)').slideToggle('slow');
+    })
+  };
+};
