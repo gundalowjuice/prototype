c382d573926ea1d70c424112781d581e999082de
Asa Smith <smith.asa.la@gmail.com>
2016-10-25 17:54:44 -0400

    mini basket drop down

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,34 @@
 console.log('you can use ES6 here : )')
 
-var hamburger = document.getElementById('hamburger');
-var menu = document.getElementById('menu');
+// var hamburger = document.getElementById('hamburger');
+// var menu = document.getElementById('menu');
+//
+// hamburger.addEventListener('click', menuToggle, false);
+//
+// function menuToggle(){
+//   menu.classList.toggle('hide');
+//   console.log('toggled');
+// };
+//
+// console.log(menu.classList);
+// console.log(hamburger);
 
-hamburger.addEventListener('click', menuToggle, false);
+$(document).ready(function(){
+  checkDisplay();
 
-function menuToggle(){
-  menu.classList.toggle('hide');
-  console.log('toggled');
+  $('#hamburger').click(function(){
+    $('#menu').fadeToggle('fast');
+  })
+});
+
+  $('#cart').click(function(){
+    $('#cart-dropdown').slideToggle('slow');
+  });
+
+function checkDisplay(){
+  if($('p:nth-of-type(2)').css('display') === 'none'){
+    $('#shipping-info').on('click', function(){
+        $('p:nth-of-type(2)').slideToggle('slow');
+    })
+  };
 };
-
-console.log(menu.classList);
-console.log(hamburger);
