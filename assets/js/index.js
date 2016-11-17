c6205ffe803999b93754f6b753d822e77daf2f23
Asa Smith <smith.asa.la@gmail.com>
2016-11-17 10:44:31 -0500

    adjust product content alignment on small screen

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,16 +14,24 @@ $(document).ready(function(){
   //   $('#basket-qty span:nth-of-type(2)').text('add to cart');
   // })
 
+// navigation drop down
+
   $('#hamburger svg').click(function(){
     $('#menu').fadeToggle('fast');
     console.log('clicked');
   })
 
+
+// initialize flickity
+
   $('.carousel').flickity({
     cellAlign: 'center',
     wrapAround: true,
-    isSelected: true
+    isSelected: true,
+    initialIndex: 0
   })
+
+// flickity
 
   var $carousel = $('.carousel').flickity();
   var flkty = $carousel.data('flickity');
@@ -32,82 +40,84 @@ $(document).ready(function(){
   $carousel.on( 'select.flickity', function() {
     var item = (flkty.selectedIndex);
     console.log(item);
-    if(item === 0){
-      $('.mainstay-green').fadeIn('slow');
-      $('.starboard-strawberry').fadeOut('fast');
-      $('.anchor-case').fadeOut('fast');
-    }else if (item === 1){
-      $('.starboard-strawberry').fadeIn('slow');
-      $('.mainstay-green').fadeOut('fast');
-      $('.anchor-case').fadeOut('fast');
-    }else if(item === 2){
-      $('.anchor-case').fadeIn('fast');
-      $('.mainstay-green').fadeOut('fast');
-      $('.starboard-strawberry').fadeOut('fast');
-    }
 
-    if(item === 0){
-      $('#nutrition').addClass('msg').removeClass('ss ac');
-    } else if(item === 1) {
-      $('#nutrition').addClass('ss').removeClass('msg ac');
-    } else if(item === 2) {
-      $('#nutrition').addClass('ac').removeClass('msg ss');
-    }
+  // change juice content based on juice display
 
-    if(item === 0){
-      $('#basket-qty > p').addClass('msg').removeClass('ss ac');
-    }else if(item === 1){
-      $('#basket-qty > p').addClass('ss').removeClass('msg ac');
-    }else if(item === 2){
-      $('#basket-qty > p').addClass('ac').removeClass('msg ss');
-    }
+      if(item === 0){
+        $('.mainstay-green').fadeIn('slow');
+        $('.starboard-strawberry').fadeOut('fast');
+        $('.anchor-case').fadeOut('fast');
+      }else if (item === 1){
+        $('.starboard-strawberry').fadeIn('slow');
+        $('.mainstay-green').fadeOut('fast');
+        $('.anchor-case').fadeOut('fast');
+      }else if(item === 2){
+        $('.anchor-case').fadeIn('slow');
+        $('.mainstay-green').fadeOut('fast');
+        $('.starboard-strawberry').fadeOut('fast');
+      }
 
-    if(item === 0){
-      $('#basket-qty span:nth-of-type(1)').addClass('msg').removeClass('ss ac');
-    }else if(item === 1){
-      $('#basket-qty span:nth-of-type(1)').addClass('ss').removeClass('msg ac');
-    }else if(item === 2){
-      $('#basket-qty span:nth-of-type(1)').addClass('ac').removeClass('msg ss');
-    }
+    // change juice nutrition info based on juice display
 
-    if(item === 0){
-      $('#ingredients h2').addClass('msg').removeClass('ss').removeClass('ac');
-    }else if(item === 1){
-      $('#ingredients h2').addClass('ss').removeClass('msg').removeClass('ac');
-    }else if(item === 2){
-      $('#ingredients h2').addClass('ac').removeClass('ss').removeClass('msg');
-    }
+      if(item === 0){
+        $('#msg-nutrition').fadeIn('slow');
+        $('#ss-nutrition').fadeOut('fast');
+        $('#ac-nutrition').fadeOut('fast');
+      } else if(item === 1) {
+        $('#ss-nutrition').fadeIn('slow');
+        $('#msg-nutrition').fadeOut('fast');
+        $('#ac-nutrition').fadeOut('fast');
+      } else if(item === 2) {
+        $('#ac-nutrition').fadeIn('slow');
+        $('#msg-nutrition').fadeOut('fast');
+        $('#ss-nutrition').fadeOut('fast');
+      }
+
+    //change basket color based on juice display
+
+      if(item === 0){
+        $('#basket-qty > p').addClass('msg').removeClass('ss ac');
+      }else if(item === 1){
+        $('#basket-qty > p').addClass('ss').removeClass('msg ac');
+      }else if(item === 2){
+        $('#basket-qty > p').addClass('ac').removeClass('msg ss');
+      }
+
+      if(item === 0){
+        $('#basket-qty span:nth-of-type(1)').addClass('msg').removeClass('ss ac');
+      }else if(item === 1){
+        $('#basket-qty span:nth-of-type(1)').addClass('ss').removeClass('msg ac');
+      }else if(item === 2){
+        $('#basket-qty span:nth-of-type(1)').addClass('ac').removeClass('msg ss');
+      }
+
+      if(item === 0){
+        $('#ingredients h2').addClass('msg').removeClass('ss').removeClass('ac');
+      }else if(item === 1){
+        $('#ingredients h2').addClass('ss').removeClass('msg').removeClass('ac');
+      }else if(item === 2){
+        $('#ingredients h2').addClass('ac').removeClass('ss').removeClass('msg');
+      }
+
+
+
+
+  // end of flickity
+
+  })
+
+// toggle nutrition labels
+
+  $('#msg-nutrition h2').click(function(){
+    $('#msg-nutrition img').slideToggle('slow');
+    console.log('clicked');
   });
 
+  $('#ss-nutrition h2').click(function(){
+    $('#ss-nutrition img').toggle('slow');
+  });
 
-
-
-  (function () {
-    var carousel, next, seats;
-    carousel = $('#carousel');
-    seats = $('.carousel-img');
-    next = function (el) {
-        if (el.next().length > 0) {
-            return el.next();
-        } else {
-            return seats.first();
-        }
-    };
-    $('.toggle').on('click', function (e) {
-        var el, i, j, new_seat, ref;
-        el = $('.is-ref').removeClass('is-ref');
-        new_seat = next(el);
-        new_seat.addClass('is-ref').css('order', 2);
-        for (i = j = 2, ref = seats.length; 2 <= ref ? j <= ref : j >= ref; i = 2 <= ref ? ++j : --j) {
-            new_seat = next(new_seat).css('order', i);
-        }
-        carousel.removeClass('is-set');
-        return setTimeout(function () {
-            return carousel.addClass('is-set');
-        }, 50);
-    });
-}.call(this));
-
+// search toggle
 
   $('#search svg').click(function(){
     $('#search input[type="text"], #search button').toggle('slow')
@@ -116,11 +126,18 @@ $(document).ready(function(){
   $('#search svg').click(function(){
     $('#search input[type="text"], #search button').toggle('slow')
   })
-});
+
+// cart drop down
 
   $('#cart').click(function(){
     $('#cart-dropdown').slideToggle('slow');
   });
+
+// end of document ready
+
+});
+
+
 
 function checkDisplay(){
   if($('p:nth-of-type(2)').css('display') === 'none'){
@@ -132,9 +149,9 @@ function checkDisplay(){
 
 
 function plus(){
-  $('#basket-qty span:nth-of-type(4)').click(function(){
+  $('#basket-qty span:nth-of-type(3)').click(function(){
     total+=7;
-    $('#basket-qty span:nth-of-type(2)').text(total);
+    $('#basket-qty span:nth-of-type(2)').text(total + ' Added to Basket');
     console.log(total);
   })
 };
@@ -143,7 +160,7 @@ function minus(){
   if(total >= 7){
     $('#basket-qty span:nth-of-type(1)').click(function(){
       total-=7;
-      $('#basket-qty span:nth-of-type(2)').text(total);
+      $('#basket-qty span:nth-of-type(2)').text(total + ' Added to Basket');
       console.log(total)
     })
   }else{
